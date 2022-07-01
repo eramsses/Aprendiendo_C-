c1d781eefccec708e331046b4d4473cd6a9e34ad
Erick R. Rodríguez <47439411+eramsses@users.noreply.github.com>
2022-07-01 05:09:30 -0600

    Proyecto Identity Framework
    Inicio del proyecto

---
+++ CursoIdentityFramework/CursoIdentityFramework/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CursoIdentityFramework.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
